5c7a559806a3a33dbf644f6cfe53e68a4403def2
solanum114 <lobrien38@outlook.com>
2025-05-09 14:09:55 +0100

    Add files via upload

---
+++ sum.f90
@@ -0,0 +1,15 @@
+program sum 
+implicit none
+
+    print *, "1 + 2 =", 1+2
+    print *, "2 + 3 =", 2+3
+    print *, "3 + 4 =", 3+4
+    print *, "4 + 5 =", 4+5
+    print *, "5 + 6 =", 5+6
+    print *, "6 + 7 =", 6+7
+    print *, "7 + 8 =", 7+8
+    print *, "8 + 9 =", 8+9
+    print *, "If we add them all together, then we get :" 
+    print *, 1+2+2+3+3+4+4+5+5+6+6+7+7+8+8+9
+
+end program sum
